6bf5fc0ddae22d22530d49ccb042526324e5c431
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-24 14:55:51 +0300

    Create a shallow copy of an array

--- js2/spread-operator.js
+++ js2/spread-operator.js
@@ -16,3 +16,38 @@ console.log();
 // Pass individual array elements into a function
 console.log(...newArr);
 console.log();
+
+// Given
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// Create menu with extra elements
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log('New menu:', newMenu);
+
+console.log();
+
+// Create a shallow copy of an array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log('Copy of main menu:', mainMenuCopy);
+z
