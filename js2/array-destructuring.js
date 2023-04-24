fddbd45f7936ef708d01a226a8b35c7d3bf0ea3c
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-24 10:36:17 +0300

    Destruct a simple array

---
+++ js2/array-destructuring.js
@@ -0,0 +1,23 @@
+// Unpacking array vaulues into seperate variables
+
+// Unpack arr
+const arr = [1, 2, 3];
+
+// Method 1
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+console.log('a:', a);
+console.log('b:', b);
+console.log('c:', c);
+
+console.log();
+
+// Method 2
+const [d, e, f] = arr;
+console.log('d:', d);
+console.log('e:', f);
+console.log('f:', f);
+
+console.log();
