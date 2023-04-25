bd24f46de7efaff84777023a3e1016d8dc0972db
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-25 09:22:45 +0300

    Short-circuit using the logical or operator

---
+++ js2/short-circuiting.js
@@ -0,0 +1,8 @@
+// Short-circuit using the logical or operator
+console.log();
+console.log(3 || 'Dan');
+console.log('' || 'Dan');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || null);
+console.log();
