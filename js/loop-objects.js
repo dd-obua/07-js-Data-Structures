ffc8cddc80e89684202f1ccd88e81c2cbfbeb51f
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-26 20:35:55 +0300

    Manipulate object property names

---
+++ js/loop-objects.js
@@ -0,0 +1,40 @@
+// Loop over propety names / keys
+
+// Given
+const days = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+];
+
+const openingHours = {
+  [days[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [days[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [days[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+console.log();
+
+const properies = Object.keys(openingHours);
+console.log(properies);
+console.log();
+
+let openStr = `We are open on ${properies.length} days, `;
+
+for (const day of properies) openStr += `${day}, `;
+console.log(openStr);
+
+console.log();
