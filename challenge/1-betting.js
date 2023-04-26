d0a6500d15b8276553a424532ed1da5a4e2d3ecf
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-26 11:10:21 +0300

    Group players by teams

---
+++ challenge/1-betting.js
@@ -0,0 +1,52 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+console.log();
+
+// Task 1: Group players by teams
+const players1 = game.players[0];
+const players2 = game.players[1];
+
+console.log('Team 1:', players1);
+console.log();
+console.log('Team 2:', players2);
+
+console.log();
