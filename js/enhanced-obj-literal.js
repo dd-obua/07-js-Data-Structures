0261ec504bf303afcf0e8956a6c51354cbe8d87e
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-26 15:28:18 +0300

    Add openingHours to resturant object using enhanced object literal

---
+++ js/enhanced-obj-literal.js
@@ -0,0 +1,31 @@
+// Given
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const resturant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
+};
+
+console.log();
+
+console.log('Resturant:', resturant);
+
+console.log();
+
