5d88924bc977482534fe59c29bded94d54a42938
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-26 17:01:13 +0300

    Log opening hours for days if they exist

---
+++ js/optional-chaining.js
@@ -0,0 +1,44 @@
+// Given
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const resturant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    return `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`;
+  },
+};
+
+console.log();
+
+// Log opening hour if property exists
+// Method 1:
+if (resturant.openingHours && resturant.openingHours.mon)
+  console.log('Mon:', resturant.openingHours.mon.open);
+
+if (resturant.openingHours && resturant.openingHours.fri)
+  console.log('Fri:', resturant.openingHours.fri.open);
+
+console.log();
