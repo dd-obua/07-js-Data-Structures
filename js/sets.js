d636412804ff6920f6a70251f5e094ce1147ffda
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-27 16:00:47 +0300

    Log and empty set

---
+++ js/sets.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+
+console.log();
+
+console.log(new Set());
+console.log();
+
