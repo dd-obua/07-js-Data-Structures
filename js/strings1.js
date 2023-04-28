fbf3dfbba7faf0c03433ade5c254b96f4f01c7c5
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-28 19:20:45 +0300

    Read length properties

--- js/strings1.js
+++ js/strings1.js
@@ -11,3 +11,8 @@ console.log('Position 1:', plane[1]);
 console.log('Position 2:', plane[2]);
 console.log('B737'[3]);
 console.log();
+
+// Read length properties
+console.log(`${airline}: ${airline.length} characters.`);
+console.log(`A320: ${'A320'.length} characters.`);
+console.log();
