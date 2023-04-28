a895319d24e8adbf9a2a20de653a7162c5580eb5
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-28 20:21:37 +0300

    Change case

---
+++ js/strings2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log();
+
+const airline = 'TAP Air Portugal';
+
+// Change case
+const lowercase = airline.toLowerCase();
+console.log('Lowercase:', lowercase);
+console.log();
+
+const uppercase = airline.toUpperCase();
+console.log('Uppercase:', uppercase);
+console.log();
