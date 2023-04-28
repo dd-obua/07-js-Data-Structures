5cfa05151b3f2a6ebc71afb09d96fb6dc69f74e9
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-28 20:31:49 +0300

    Fix capitalization using a function

--- js/strings2.js
+++ js/strings2.js
@@ -12,3 +12,15 @@ console.log();
 const uppercase = airline.toUpperCase();
 console.log('Uppercase:', uppercase);
 console.log();
+
+// Fix capilization in the name
+const passenger = 'dEnIS';
+
+function toSentenceCase(word) {
+  const lowercase = word.toLowerCase();
+  const correctCase = lowercase[0].toUpperCase() + lowercase.slice(1);
+  return correctCase;
+}
+
+console.log(toSentenceCase(passenger));
+console.log();
