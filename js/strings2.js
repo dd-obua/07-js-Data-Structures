5ce87f363863d202c13a11c17cd73818ef75f397
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-29 14:21:03 +0300

    Replace first characters of words using [] indexing

--- js/strings2.js
+++ js/strings2.js
@@ -125,9 +125,7 @@ console.log();
 const capitalizeName = function (name) {
   const names = name.split(' ');
   const result = [];
-  names.forEach(nm =>
-    result.push(nm.replace(nm.charAt(0), nm.charAt(0).toUpperCase()))
-  );
+  names.forEach(nm => result.push(nm.replace(nm[0], nm[0].toUpperCase())));
   return result.join(' ');
 };
 
