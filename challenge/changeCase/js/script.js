d388d37ca5ca99e5231011e9d87f4e37f11db68c
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-29 20:25:17 +0300

    Style textarea and button

--- challenge/changeCase/js/script.js
+++ challenge/changeCase/js/script.js
@@ -3,3 +3,8 @@ document.body.append(document.createElement('button'));
 
 const textArea = document.querySelector('textarea');
 const btn = document.querySelector('button');
+
+textArea.style.width = '500px';
+textArea.style.height = '200px';
+textArea.style.display = 'block';
+textArea.style.marginBottom = '12px';
