adee0aa8268c3eb7a04a47b2689ccb882d6eb9d4
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-29 20:24:48 +0300

    Select textarea and button

--- challenge/changeCase/js/script.js
+++ challenge/changeCase/js/script.js
@@ -1,2 +1,5 @@
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
+
+const textArea = document.querySelector('textarea');
+const btn = document.querySelector('button');
