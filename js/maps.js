f05adc853948d1897a92ce24c469b9f1405c6ab7
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-01 14:45:12 +0300

    Add days array and openingHours object

--- js/maps.js
+++ js/maps.js
@@ -2,6 +2,32 @@
 
 console.log();
 
+const days = [
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+  'sunday',
+];
+
+// Compute property names
+const openingHours = {
+  [days[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [days[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [days[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Maps
 const resturant = new Map();
 console.log(resturant);
@@ -11,4 +37,3 @@ resturant.set('name', 'Classico Italiano');
 resturant.set(1, 'Firenz, Itally');
 console.log(resturant.set(2, 'Lisbon, Portugal')); // (Call set method returns the map)
 console.log();
-
