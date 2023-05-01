73c42d3f80377f4000266015cc767699fcb4bd49
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-01 14:47:54 +0300

    Replace openingHours with resturant object and rename resturant map

--- js/maps.js
+++ js/maps.js
@@ -12,28 +12,36 @@ const days = [
   'sunday',
 ];
 
-// Compute property names
-const openingHours = {
-  [days[3]]: {
-    open: 12,
-    close: 22,
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
   },
-  [days[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [days[5]]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
+}
 
 // Maps
-const resturant = new Map();
-console.log(resturant);
+const resturantMap = new Map();
+console.log(resturantMap);
 
 // Set map values
-resturant.set('name', 'Classico Italiano');
-resturant.set(1, 'Firenz, Itally');
-console.log(resturant.set(2, 'Lisbon, Portugal')); // (Call set method returns the map)
+resturantMap.set('name', 'Classico Italiano');
+resturantMap.set(1, 'Firenz, Itally');
+console.log(resturantMap.set(2, 'Lisbon, Portugal')); // (Call set method returns the map)
 console.log();
