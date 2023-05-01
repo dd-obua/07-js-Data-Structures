ae16ec802362f57498be37b8e6b5314247b61ae2
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-01 14:31:17 +0300

    Set map values

--- js/maps.js
+++ js/maps.js
@@ -5,3 +5,10 @@ console.log();
 // Maps
 const resturant = new Map();
 console.log(resturant);
+
+// Set map values
+resturant.set('name', 'Classico Italiano');
+resturant.set(1, 'Firenz, Itally');
+console.log(resturant.set(2, 'Lisbon, Portugal')); // (Call set method returns the map)
+console.log();
+
